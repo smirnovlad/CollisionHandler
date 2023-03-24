a90c8126db4543fe3870e2014c8fbd97052e0a8f
Smirnov Vlad <smirnov.vladislav@phystech.edu>
2023-03-24 10:42:02 +0300

    some freezes when using heap

--- BallCollision/lib/QuadTree.h
+++ BallCollision/lib/QuadTree.h
@@ -21,27 +21,29 @@ class QuadTree {
   struct Node {
     std::array<std::unique_ptr<Node>, 4> children;
     AABB boundary;
-    std::vector<Ball> balls;
+    std::vector<uint32_t> balls_id;
 
     Node() = default;
     Node(const AABB &boundary) :
         boundary(boundary) {}
 
     void subdivide();
-    bool insert(const Ball &ball);
-    void query(const AABB &range,
-               std::vector<Ball> &close_balls);
+    bool insert(QuadTree *qt_ptr, uint32_t idx);
+    void query(QuadTree *qt_ptr,
+               const AABB &range,
+               std::vector<uint32_t> &close_balls_id);
   };
 
-  std::vector<Ball> query(const AABB &range);
+  std::vector<uint32_t> query(const AABB &range);
  public:
-  QuadTree(const AABB &boundary);
-  bool insert(const Ball &ball);
-  std::vector<Ball> get_close_balls(const Ball &ball);
+  QuadTree(const std::vector<Ball> &balls,
+           const AABB &boundary);
+  std::vector<uint32_t> get_close_balls(const Ball &ball);
 
  private:
   std::unique_ptr<Node> root_{nullptr};
-  const static uint32_t MAX_AABB_BALLS_COUNT = 3;
+  std::vector<Ball> balls_;
+  const uint32_t MAX_AABB_BALLS_COUNT = 3;
 };
 
 #endif //INTERVIEW_BALLCOLLISION_QUADTREE_H_
--- BallCollision/src/QuadTree.cpp
+++ BallCollision/src/QuadTree.cpp
@@ -24,8 +24,13 @@ bool QuadTree::AABB::intersects(const AABB &other) const {
   }
 }
 
-QuadTree::QuadTree(const AABB &boundary) {
+QuadTree::QuadTree(const std::vector<Ball> &balls,
+                   const AABB &boundary) : balls_(balls) {
   root_ = std::make_unique<Node>(boundary);
+  uint32_t balls_count = balls.size();
+  for (uint32_t i = 0; i < balls_count; ++i) {
+    root_->insert(this, i);
+  }
 }
 
 void QuadTree::Node::subdivide() {
@@ -48,15 +53,11 @@ void QuadTree::Node::subdivide() {
            quarter_length});
 }
 
-bool QuadTree::insert(const Ball &ball) {
-  return root_->insert(ball);
-}
-
-bool QuadTree::Node::insert(const Ball &ball) {
-  if (boundary.contains(ball)) {
+bool QuadTree::Node::insert(QuadTree *qt_ptr, uint32_t idx) {
+  if (boundary.contains(qt_ptr->balls_[idx])) {
     if (children[0] == nullptr) {
-      if (balls.size() < MAX_AABB_BALLS_COUNT) {
-        balls.push_back(ball);
+      if (balls_id.size() < qt_ptr->MAX_AABB_BALLS_COUNT) {
+        balls_id.push_back(idx);
         return true;
       }
       subdivide();
@@ -64,38 +65,39 @@ bool QuadTree::Node::insert(const Ball &ball) {
     }
     bool is_inserted = false;
     for (uint32_t i = 0; i < 4; ++i) {
-      is_inserted |= children[i]->insert(ball);
+      is_inserted |= children[i]->insert(qt_ptr, idx);
     }
     return is_inserted;
   }
 }
 
-std::vector<Ball> QuadTree::get_close_balls(const Ball &ball) {
+std::vector<uint32_t> QuadTree::get_close_balls(const Ball &ball) {
   auto search_range = AABB{ball.get_center().x,
                            ball.get_center().y,
                            ball.get_radius() + 1};
   return query(search_range);
 }
 
-std::vector<Ball> QuadTree::query(const AABB &range) {
-  std::vector<Ball> close_balls;
-  root_->query(range, close_balls);
+std::vector<uint32_t> QuadTree::query(const AABB &range) {
+  std::vector<uint32_t> close_balls;
+  root_->query(this, range, close_balls);
   return close_balls;
 }
 
-void QuadTree::Node::query(const AABB &range,
-                           std::vector<Ball> &close_balls) {
+void QuadTree::Node::query(QuadTree *qt_ptr,
+                           const AABB &range,
+                           std::vector<uint32_t> &close_balls) {
   if (!boundary.intersects(range)) {
     return;
   }
-  for (const auto &ball : balls) {
-    if (range.contains(ball)) {
-      close_balls.push_back(ball);
+  for (uint32_t id : balls_id) {
+    if (range.contains(qt_ptr->balls_[id])) {
+      close_balls.push_back(id);
     }
   }
   if (children[0] != nullptr) {
     for (uint32_t i = 0; i < 4; ++i) {
-      children[i]->query(range, close_balls);
+      children[i]->query(qt_ptr, range, close_balls);
     }
   }
 }
