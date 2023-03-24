e802c4ba24c2e9fb793c4f40361fc0f89121626c
unknown <xokerok@mail.ru>
2023-03-24 20:19:14 +0300

    windows OS support added

--- BallCollision/lib/constants.h
+++ BallCollision/lib/constants.h
@@ -1,14 +1,14 @@
 #ifndef INTERVIEW_BALLCOLLISION_CONSTANTS_H_
 #define INTERVIEW_BALLCOLLISION_CONSTANTS_H_
 
-constexpr int WINDOW_X = 1024;
-constexpr int WINDOW_Y = 700;
-constexpr int MIN_BALLS = 14;
-constexpr int MAX_BALLS = 20;
+constexpr int WINDOW_X = 1920;
+constexpr int WINDOW_Y = 1080;
+constexpr int MIN_BALLS = 30;
+constexpr int MAX_BALLS = 50;
 constexpr double MIN_BALL_R = 40;
 constexpr double MAX_BALL_R = 60;
-constexpr double MIN_BALL_SPEED = 80;
-constexpr double MAX_BALL_SPEED = 100;
+constexpr double MIN_BALL_SPEED = 150;
+constexpr double MAX_BALL_SPEED = 200;
 constexpr int DIR_SCALE = 100;
 
 #endif //INTERVIEW_BALLCOLLISION_CONSTANTS_H_
